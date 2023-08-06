9e8a2febdf85bf3ff4d73c3ef964bfc989378a34
gominzip <ahaminji@naver.com>
2023-08-07 01:50:30 +0900

    [CHORE] 테스트를 위해 상단바 로고이동 처리

--- src/components/community/CommunityNavBar.jsx
+++ src/components/community/CommunityNavBar.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { styled } from "styled-components";
 
 const CommunityNavBar = () => {
+  const navigate = useNavigate();
+
   return (
     <Wrapper>
-      <LogoWrapper>
+      <LogoWrapper onClick={() => navigate(`/`)}>
         <LogoImage src="/img/navlogo.png" />
       </LogoWrapper>
       <ButtonWrapper>
