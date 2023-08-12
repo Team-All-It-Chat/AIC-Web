167a4ce8cfebbe88e5ba4782cc4143ab089ef877
gominzip <ahaminji@naver.com>
2023-08-13 01:49:48 +0900

    [FEAT] 꿀팁게시물 작성 페이지 추가

---
+++ src/pages/WritePostPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CommunityNavBar from "../components/community/CommunityNavBar";
+import WritePostSection from "../components/writePost/WritePostSection";
+
+const WritePostPage = () => {
+  return (
+    <>
+      <CommunityNavBar />
+      <WritePostSection />
+    </>
+  );
+};
+
+export default WritePostPage;
