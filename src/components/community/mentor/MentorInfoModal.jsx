fad7744fdfc47e2734d8b16cd1da01146330765a
gominzip <ahaminji@naver.com>
2023-08-14 22:10:58 +0900

    [CICD] api 호출 테스트

--- src/components/community/mentor/MentorInfoModal.jsx
+++ src/components/community/mentor/MentorInfoModal.jsx
@@ -1,25 +1,25 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import MentorCard from "./MentorCard";
 import { styled } from "styled-components";
-import { getMentorProfiles } from "../../../apis/accounts";
-import { useParams } from "react-router-dom";
+// import { getMentorProfiles } from "../../../apis/accounts";
+// import { useParams } from "react-router-dom";
 
 const MentorInfoModal = () => {
   const dataLength = 9;
-  const { continent } = useParams();
-  const [mentorListData, setMentorList] = useState([]);
+  // const { continent } = useParams();
+  // const [mentorListData, setMentorList] = useState([]);
 
-  // 렌더 전 데이터 패칭을 위한 useEffect
-  useEffect(() => {
-    const fetchData = async () => {
-      // console.log(continent);
-      const response = await getMentorProfiles(continent);
-      setMentorList(response.data.result);
-      console.log(response.data.result);
-      console.log(mentorListData);
-    };
-    fetchData();
-  }, []);
+  // // 렌더 전 데이터 패칭을 위한 useEffect
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     // console.log(continent);
+  //     const response = await getMentorProfiles(continent);
+  //     setMentorList(response.data.result);
+  //     console.log(response.data.result);
+  //     console.log(mentorListData);
+  //   };
+  //   fetchData();
+  // }, []);
 
   return (
     <Wrapper>
