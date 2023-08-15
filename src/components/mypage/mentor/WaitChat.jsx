2264d83643f034abfd5f60b66dbacd0680de983f
CJY <cjy3458@naver.com>
2023-08-15 22:37:54 +0900

    [FEAT] 링크 주소 해당 질문의 ID에 맞게 변경

--- src/components/mypage/mentor/WaitChat.jsx
+++ src/components/mypage/mentor/WaitChat.jsx
@@ -11,7 +11,7 @@ const WaitChat = ({ database }) => {
       : database.question;
 
   const onClick = () => {
-    navigate(`/applyAlert`);
+    navigate(`/applyAlert/${database.id}`);
   };
   return (
     <>
