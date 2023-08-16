b1003d614c9a4693af8b75f9154e8b030b47ebe3
gominzip <ahaminji@naver.com>
2023-08-16 19:33:34 +0900

    [CICD] 불필요한 import 삭제

--- src/components/writePost/WritePostSection.jsx
+++ src/components/writePost/WritePostSection.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { json, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { styled } from "styled-components";
 import { useForm } from "../../hooks/useForm";
 import { postNewTip } from "../../apis/posts";
