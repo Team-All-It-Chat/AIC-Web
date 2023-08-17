765e0b71ac06294dc1c793c92aa5092c106c33e9
gominzip <ahaminji@naver.com>
2023-08-18 00:34:04 +0900

    [FEAT] 멘토 마이페이지 게시글 관리 모달수정

--- src/components/mypage/mentor/PostsModal.jsx
+++ src/components/mypage/mentor/PostsModal.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { styled } from "styled-components";
-import PostCard from "./PostCard";
 import { useNavigate } from "react-router-dom";
 import { getAllPost } from "../../../apis/posts";
 import PostDataSectionMypage from "./PostDataSectionMypage";
